a5739e96224afacaa268f775c6771e7aef5df6a1
Daniel Canedo <dnl.canedo@gmail.com>
2020-12-14 14:52:01 -0400

    Extend greetings

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -1,7 +1,10 @@
 package greetings;
 
-import  ( "fmt"
-"errors"
+import  ( 
+	"fmt"
+	"errors"
+	"math/rand"
+	"time"
 )
 
 // Hello returns a greeting for the named person.
@@ -11,7 +14,37 @@ func Hello(name string) (string, error) {
 	}
 
 	// Return a greeting that embeds the name in a message
-	message := fmt.Sprintf("Hi %v, welcome!", name);
+	message := fmt.Sprintf(getRandomMessage(), name);
 
 	return message, nil
+}
+
+func Hellos(names []string) (map[string]string, error) {
+	messages := make(map[string]string)
+
+	for _, name := range names {
+		message, err := Hello(name)
+
+		if err != nil {
+			return nil, err
+		}
+
+		messages[name] = message
+	}
+
+	return messages, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func getRandomMessage() string {
+	strings := []string{
+		"Hi %v, nice to see you",
+		"What's up %v?",
+		"Nice to meet you %v",
+	}
+
+	return strings[rand.Intn(len(strings))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -11,13 +11,26 @@ func main() {
 	log.SetPrefix("Greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Priscila")
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(message)
+
+	names := []string{ "Monica", "Pilar", "Ramona" }
+	messages, err := greetings.Hellos(names)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// for i := 1; i < len(names); i++ {
+	// 	fmt.Println(messages[names[i]])
+	// }
+
+	fmt.Println(messages)
 
 	sumResult := math.Sum(1, 2)
 	fmt.Printf("1 + 2 = %v\n", sumResult)
