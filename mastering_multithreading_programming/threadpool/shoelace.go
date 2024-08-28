47d114c05e99af9782878418d9c35a927af225c5
Daniel Canedo <dnl.canedo@gmail.com>
2024-08-28 18:41:59 -0400

    MMP: 20: Thread Pool Example Part 2

--- mastering_multithreading_programming/threadpool/shoelace.go
+++ mastering_multithreading_programming/threadpool/shoelace.go
@@ -8,6 +8,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -17,24 +18,29 @@ type Point2D struct {
 }
 
 var r = regexp.MustCompile(`\((\d*),(\d*)\)`)
+var numberOfThreads = 8
+var waitGroup = sync.WaitGroup{}
 
-func findArea(pointsStr string) float64 {
-	var points []Point2D
-	area := 0.0
+func findArea(inputChannel chan string) {
+	for pointsStr := range inputChannel {
+		var points []Point2D
+		area := 0.0
 
-	for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
-		x, _ := strconv.Atoi(p[1])
-		y, _ := strconv.Atoi(p[2])
-		points = append(points, Point2D{ x, y })
+		for _, p := range r.FindAllStringSubmatch(pointsStr, -1) {
+			x, _ := strconv.Atoi(p[1])
+			y, _ := strconv.Atoi(p[2])
+			points = append(points, Point2D{ x, y })
+		}
+
+		for i := 0; i < len(points); i++ {
+			a, b := points[i], points[(i + 1) % len(points)]
+			area += float64(a.x * b.y) - float64(a.y * b.x)
+		}
+
+		fmt.Println(math.Abs(area) / 2.0)
 	}
 
-	for i := 0; i < len(points); i++ {
-		a, b := points[i], points[(i + 1) % len(points)]
-		area += float64(a.x * b.y) - float64(a.y * b.x)
-	}
-
-
-	return math.Abs(area) / 2.0
+	waitGroup.Done()
 }
 
 func main() {
@@ -43,11 +49,19 @@ func main() {
 	file, _ := ioutil.ReadFile(filePath)
 	text := string(file)
 
-	for _, line := range strings.Split(text , "\n") {
-		res := findArea(line)
-		fmt.Println(res)
+	inputChannel := make(chan string, 1000)
+
+	for i := 0; i < numberOfThreads; i ++ {
+		waitGroup.Add(1)
+		go findArea(inputChannel)
 	}
 
+	for _, line := range strings.Split(text , "\n") {
+		inputChannel <- line
+	}
+
+	close(inputChannel)
+	waitGroup.Wait()
 	timeElapsed := time.Since(start)
 	fmt.Printf("Time elapsed: %v\n", timeElapsed)
 }
