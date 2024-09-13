87e57c847e2440d56d4051326a2e442c51c0a2eb
Daniel Canedo <dnl.canedo@gmail.com>
2024-09-12 20:15:19 -0400

    MMP: 40: Using Atomic Variables in our example

--- mastering_multithreading_programming/atomicletterfrecuency/letter_frecuency.go
+++ mastering_multithreading_programming/atomicletterfrecuency/letter_frecuency.go
@@ -5,35 +5,49 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strings"
+	"sync"
+	"sync/atomic"
 	"time"
 )
 
 const allLetters = "abcdefghijklmnopqrstuvwxyz"
+var lock = sync.Mutex{}
+var waitGroup = sync.WaitGroup{}
+var threads = 8
 
-func countFrecuency(url string, frecuency *[26]int) {
+func countFrecuency(url string, frecuency *[26]int64) {
 	response, _ := http.Get(url)
 	defer response.Body.Close()
 	body, _ := ioutil.ReadAll(response.Body)
 
-	for _, b := range body {
-		letter := strings.ToLower(string(b))
-		index := strings.Index(allLetters, letter)
+	for i := 0; i < 20; i++ {
+		for _, b := range body {
+			letter := strings.ToLower(string(b))
+			index := strings.Index(allLetters, letter)
 
-		if index >= 0 {
-			frecuency[index] += 1
+			if index >= 0 {
+				// lock.Lock()
+				atomic.AddInt64(&frecuency[index], 1)
+				// frecuency[index] += 1
+				// lock.Unlock()
+			}
 		}
 	}
+
+	waitGroup.Done()
 }
 
 func main() {
-	var frecuency [26]int
+	var frecuency [26]int64
 	start := time.Now()
 
 	for i := 1000; i <= 1200; i += 1 {
 		url := fmt.Sprintf("https://rfc-editor.org/rfc/rfc%v.txt", i)
-		countFrecuency(url, &frecuency)
+		waitGroup.Add(1)
+		go countFrecuency(url, &frecuency)
 	}
 
+	waitGroup.Wait()
 	elapsed := time.Since(start)
 	fmt.Println("Done")
 	fmt.Printf("Processing took %s\n", elapsed)
